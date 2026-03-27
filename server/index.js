c758903882c6e3fac5576301ead4d8369f9a23ce
fr3d <fr3d@gmail.com>
2026-03-27 11:11:16 -0400

    Refactor to multi-game structure: add game selector, route games from /games/:name

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const gamesPath = path.join(__dirname, '../public/games');
+
+function getAvailableGames() {
+    try {
+        const dirs = fs.readdirSync(gamesPath);
+        return dirs.filter(dir => {
+            const stat = fs.statSync(path.join(gamesPath, dir));
+            return stat.isDirectory();
+        });
+    } catch (e) {
+        return [];
+    }
+}
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -10,6 +25,24 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/games/:gameName', (req, res) => {
+    const gameName = req.params.gameName;
+    const gamePath = path.join(gamesPath, gameName);
+    
+    if (fs.existsSync(gamePath)) {
+        const indexPath = path.join(gamePath, 'index.html');
+        if (fs.existsSync(indexPath)) {
+            return res.sendFile(indexPath);
+        }
+    }
+    
+    res.redirect('/');
+});
+
+app.use((req, res) => {
+    res.redirect('/');
+});
+
 app.listen(PORT, () => {
-    console.log(`Grocery Gauntlet running on http://localhost:${PORT}`);
+    console.log(`Game Center running on http://localhost:${PORT}`);
 });
